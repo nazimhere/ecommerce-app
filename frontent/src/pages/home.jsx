9e66837e6e37d05222ce9320d2dee04bebdd1fdd
Nazim <nazimkhan0668@gmail.com>
2026-02-22 23:48:09 +0530

    just started on ecommerce project

---
+++ frontent/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/hero'
+import LatestCollection from '../components/latestCollection'
+
+const Home = () => {
+  return (
+     <div >
+      <Hero/> 
+      <LatestCollection/>
+    </div>
+  )
+}
+
+export default Home;

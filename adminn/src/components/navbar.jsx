77b672e7321462954b1a300af027fa5562e27459
Nazim <nazimkhan0668@gmail.com>
2026-03-05 00:42:24 +0530

    admin change

---
+++ adminn/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <>
+    
+      <button>Logout</button>
+    </>
+  )
+}
+export default Navbar

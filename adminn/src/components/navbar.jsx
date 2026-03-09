fab0f0df7ff1c6eb9edcdabad104ed02e941f1ab
Nazim <nazimkhan0668@gmail.com>
2026-03-10 02:11:12 +0530

    vercel connected all

--- adminn/src/components/navbar.jsx
+++ adminn/src/components/navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import {lo} from '../assets/assets'
 
 const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
-    <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+    <img className='w-80' src={lo} alt="" />
       <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:px-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
